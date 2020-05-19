7ef1f9f4dc1fe6c0b933c42594e60f42435ff77b
hangzhouwh <hangzhouwh@gmail.com>
2020-05-19 08:18:50 +0800

    ...

--- src/main/java/cn/edu/zucc/syx/rec/entity/RecordSong.java
+++ src/main/java/cn/edu/zucc/syx/rec/entity/RecordSong.java
@@ -3,7 +3,6 @@ package cn.edu.zucc.syx.rec.entity;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
 public class RecordSong {
