ed047204978b93f870ff33ac34e29ec2e5321494
syxaa <55424257+syxaa@users.noreply.github.com>
2020-05-19 20:27:00 +0800

    update recommend page

--- src/main/java/cn/edu/zucc/syx/rec/controller/RecommendController.java
+++ src/main/java/cn/edu/zucc/syx/rec/controller/RecommendController.java
@@ -3,8 +3,12 @@ package cn.edu.zucc.syx.rec.controller;
 import cn.edu.zucc.syx.rec.entity.KeySong;
 import cn.edu.zucc.syx.rec.service.RecommendService;
 import cn.edu.zucc.syx.rec.util.JsonUtil;
+import cn.edu.zucc.syx.rec.util.PageUtil;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,9 +24,19 @@ public class RecommendController {
 
     // 推荐1, deeplearning
     @GetMapping("/{host}/recommandbyDl")
-    public JSONObject recommandSongByDl(@PathVariable("host") String host){
-        List<KeySong> recommendDLsongs = recommendService.recommandSongByDl(host);
-        JSONObject ret  = util.userSongs2Json(recommendDLsongs);
+    public JSONObject recommandSongByDl(@PathVariable("host") String host, @RequestParam("page_num") int pageNum,
+                                        @RequestParam("page_size") int pageSize){
+        JSONObject ret = new JSONObject();
+
+        try{
+            List<KeySong> recommendDLsongs = recommendService.recommandSongByDl(host);
+            Pageable pageable = PageRequest.of(pageNum-1, pageSize);
+            Page<KeySong> page = PageUtil.createPageFromList(recommendDLsongs, pageable);
+            ret = util.collectionSongPage2Json(page);
+        } catch (Exception e){
+            ret.put("code", "error");
+            ret.put("msg", "failed");
+        }
         return ret;
     }
 }
