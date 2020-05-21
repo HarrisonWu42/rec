e606b30f87222048b409a7ff7190a3028653a521
hangzhouwh <hangzhouwh@gmail.com>
2020-05-21 09:48:43 +0800

    部署ip

--- src/main/java/cn/edu/zucc/syx/rec/controller/RecommendController.java
+++ src/main/java/cn/edu/zucc/syx/rec/controller/RecommendController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://39.101.189.21:8888", maxAge = 3600)
 @CrossOrigin(origins = "http://localhost:8888", maxAge = 3600)
 @RestController
 @RequestMapping("/api/rec")
